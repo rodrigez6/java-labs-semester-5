1908efc5d0d7f66188de05b09a0155c0959dded8
mykola <rodrigez6@ukr.net>
2018-11-25 06:00:18 +0200

    say no to c-style

--- src/main/java/org/rodrigez/app/Demo.java
+++ src/main/java/org/rodrigez/app/Demo.java
@@ -13,7 +13,7 @@ public class Demo {
 
     private static DispatcherController controller;
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         configure();
         addTestEmployees();
         controller.runMenu();
